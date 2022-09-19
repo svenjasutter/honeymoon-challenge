07a0f4396b4d617a390240191feddc21f90015ea
svenjasutter <svenja.sutter2@gmail.com>
2022-09-19 15:24:35 +0200

    testing gallery

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -11,37 +11,42 @@ import { SupabaseService } from 'app/supabase.service';
 export class GalleryComponent implements OnInit {
   images :any[] = [];
   image :any;
+  // imagess :Promise<string[]>;
+
   constructor(
     private storageService :StorageService,
     private supabase :SupabaseService
     ) { }
 
   ngOnInit(): void {
-    console.log("gallery component");
-    this.storageService.download('photos', 'test.jpg').then((response) =>{
-      console.log(response.data);
+
+    this.testPhotos();
+
+    // this.supabase.getAllChallenges().then(ret=>{
+    //   ret.forEach(challenge=>{
+    //     if(challenge.path != "" && challenge.path != null){
+    //       console.log(challenge.path);
+    //       //display every image
+    //     }
+    //   })
+    // })
+
+  }
+
+  async testPhotos(){
+    await this.storageService.download('photos', 'test.jpg').then((response) =>{
       const reader = new FileReader();
       reader.readAsDataURL(response.data);
       reader.onload = _event => {
-        console.log("reader result: " + reader.result);
+        // console.log("reader result: " + reader.result);
+
         this.images.push(reader.result);
-        console.log("list:" + this.images);
+        console.log('list:', this.images)
+        
         this.image = reader.result;
-        console.log("single:" + this.image);
-      // this.image.nativeElement.src = url; //image declared earlier
+        console.log('single:' + this.image);
       };
     });
-
-
-    this.supabase.getAllChallenges().then(ret=>{
-      ret.forEach(challenge=>{
-        if(challenge.path != "" && challenge.path != null){
-          console.log(challenge.path);
-          //display every image
-        }
-      })
-    })
-
   }
 
 }
